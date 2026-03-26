10477d53fdf5c3671d1ea2003f20d5bbf08f8add
Rakshana M <rakshanambsc24@rvu.edu.in>
2026-03-26 12:14:18 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,6 @@
+name = input("Enter name: ")
+cls = input("Enter class: ")
+section = input("Enter section: ")
+usn = input("Enter usn: ")
+
+print("Hello \n", "My name is ", name, "\n I am from class ", cls, "\n Section ", section, "\n USN ", usn)
